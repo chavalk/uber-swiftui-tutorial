e1cf7a05f056b9d96721dbcee0f0fbd83ba65d91
chavalk <chavalk@hotmail.com>
2023-04-06 11:31:34 -0500

    First commit.

---
+++ UberSwiftUITutorial/UberSwiftUITutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  UberSwiftUITutorialApp.swift
+//  UberSwiftUITutorial
+//
+//  Created by Jose Garcia on 4/6/23.
+//
+
+import SwiftUI
+
+@main
+struct UberSwiftUITutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
