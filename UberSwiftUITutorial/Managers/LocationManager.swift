3126fd762f0637daadc5e739d4776d164bc51b83
chavalk <chavalk@hotmail.com>
2023-04-06 18:40:47 -0500

    Add location manager.

---
+++ UberSwiftUITutorial/Managers/LocationManager.swift
@@ -0,0 +1,18 @@
+//
+//  LocationManager.swift
+//  UberSwiftUITutorial
+//
+//  Created by Jose Garcia on 4/6/23.
+//
+
+import CoreLocation
+
+class LocationManager: NSObject, ObservableObject {
+    private let locationManager = CLLocationManager()
+    
+    override init() {
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
+    }
+}
