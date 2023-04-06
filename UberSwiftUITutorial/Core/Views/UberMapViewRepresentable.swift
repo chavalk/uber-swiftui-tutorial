a1ef4dcd174e2e9b3e412e21050778026730b84d
chavalk <chavalk@hotmail.com>
2023-04-06 14:20:35 -0500

    Add functions make ui view, update ui view, and make coordinator.

--- UberSwiftUITutorial/Core/Views/UberMapViewRepresentable.swift
+++ UberSwiftUITutorial/Core/Views/UberMapViewRepresentable.swift
@@ -11,4 +11,20 @@ import MapKit
 struct UberMapViewRepresentable: UIViewRepresentable {
     
     let mapView = MKMapView()
+    
+    func makeUIView(context: Context) -> some UIView {
+        mapView.isRotateEnabled = false
+        mapView.showsUserLocation = true
+        mapView.userTrackingMode = .follow
+        
+        return mapView
+    }
+    
+    func updateUIView(_ uiView: UIViewType, context: Context) {
+        
+    }
+    
+    func makeCoordinator() -> MapCoordinator {
+        
+    }
 }
