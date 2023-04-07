3a0edfdc13ed93d9da6623175fad7683c53704d2
chavalk <chavalk@hotmail.com>
2023-04-06 19:11:51 -0500

    Add CL Location manager delegate extension to location manager and set delegate.

--- UberSwiftUITutorial/Managers/LocationManager.swift
+++ UberSwiftUITutorial/Managers/LocationManager.swift
@@ -11,8 +11,16 @@ class LocationManager: NSObject, ObservableObject {
     private let locationManager = CLLocationManager()
     
     override init() {
+        locationManager.delegate = self
         locationManager.desiredAccuracy = kCLLocationAccuracyBest
         locationManager.requestWhenInUseAuthorization()
         locationManager.startUpdatingLocation()
+    }
+}
+
+extension LocationManager: CLLocationManagerDelegate {
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        guard !locations.isEmpty else { return }
+        locationManager.stopUpdatingLocation()
     }
 }
