5e90573e09c48644f210ddce3be67f91fd9a06f6
chavalk <chavalk@hotmail.com>
2023-04-07 20:14:01 -0500

    Add show location search view binding var.

--- UberSwiftUITutorial/Core/Location Search/Views/LocationSearchView.swift
+++ UberSwiftUITutorial/Core/Location Search/Views/LocationSearchView.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 struct LocationSearchView: View {
     @State private var startLocationText = ""
+    @Binding var showLocationSearchView: Bool
     @StateObject var viewModel = LocationSearchViewModel()
     
     var body: some View {
@@ -52,6 +53,9 @@ struct LocationSearchView: View {
                 VStack(alignment: .leading) {
                     ForEach(viewModel.results, id: \.self) { result in
                         LocationSearchResultsCell(title: result.title, subtitle: result.subtitle)
+                            .onTapGesture {
+                                showLocationSearchView.toggle()
+                            }
                     }
                 }
             }
@@ -62,6 +66,6 @@ struct LocationSearchView: View {
 
 struct LocationSearchView_Previews: PreviewProvider {
     static var previews: some View {
-        LocationSearchView()
+        LocationSearchView(showLocationSearchView: .constant(false))
     }
 }
