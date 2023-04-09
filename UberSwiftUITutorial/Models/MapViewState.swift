b8a4f3d4665885e913bbcc76d016269611d36153
chavalk <chavalk@hotmail.com>
2023-04-09 11:23:19 -0500

    Add map view state and implement it throghout app to manage state instead of show location binding var.

---
+++ UberSwiftUITutorial/Models/MapViewState.swift
@@ -0,0 +1,14 @@
+//
+//  MapViewState.swift
+//  UberSwiftUITutorial
+//
+//  Created by Jose Garcia on 4/9/23.
+//
+
+import Foundation
+
+enum MapViewState {
+    case noInput
+    case searchingForLocation
+    case locatitonSelected
+}
