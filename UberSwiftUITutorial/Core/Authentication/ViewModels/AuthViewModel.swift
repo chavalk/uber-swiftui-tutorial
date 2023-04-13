2760b64c50473d25a68860263c0508c8ee844a56
chavalk <chavalk@hotmail.com>
2023-04-12 20:20:10 -0500

    Add sign in and sign out functions.

--- UberSwiftUITutorial/Core/Authentication/ViewModels/AuthViewModel.swift
+++ UberSwiftUITutorial/Core/Authentication/ViewModels/AuthViewModel.swift
@@ -15,6 +15,17 @@ class AuthViewModel: ObservableObject {
         userSession = Auth.auth().currentUser
     }
     
+    func signIn(withEmail email: String, password: String) {
+        Auth.auth().signIn(withEmail: email, password: password) { result, error in
+            if let error = error {
+                print("DEBUG: Failed to sign in with error \(error.localizedDescription)")
+                return
+            }
+            
+            self.userSession = result?.user
+        }
+    }
+    
     func registerUser(withEmail email: String, password: String, fullName: String) {
         Auth.auth().createUser(withEmail: email, password: password) { result, error in
             if let error = error {
@@ -22,8 +33,16 @@ class AuthViewModel: ObservableObject {
                 return
             }
             
-            print("DEBUG: Registered user successfully")
-            print("DEBUG: User id \(result?.user.uid)")
+            self.userSession = result?.user
+        }
+    }
+    
+    func signOut() {
+        do {
+            try Auth.auth().signOut()
+            self.userSession = nil
+        } catch let error {
+            print("DEBUG: Failed to sign out with error: \(error.localizedDescription)")
         }
     }
 }
--- UberSwiftUITutorial/Core/Authentication/Views/LoginView.swift
+++ UberSwiftUITutorial/Core/Authentication/Views/LoginView.swift
@@ -10,6 +10,7 @@ import SwiftUI
 struct LoginView: View {
     @State var email = ""
     @State var password = ""
+    @EnvironmentObject var viewModel: AuthViewModel
     
     var body: some View {
         NavigationStack {
@@ -99,7 +100,7 @@ struct LoginView: View {
                     
                     // Sign in button
                     Button {
-                        
+                        viewModel.signIn(withEmail: email, password: password)
                     } label: {
                         HStack {
                             Text("SIGN IN")
