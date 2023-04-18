182bd53e04675daeec3655f6153970694b393e37
chavalk <chavalk@hotmail.com>
2023-04-18 11:05:11 -0500

    Add home view model with fetch drivers function.

---
+++ UberSwiftUITutorial/Core/Home/ViewModel/HomeViewModel.swift
@@ -0,0 +1,26 @@
+//
+//  HomeViewModel.swift
+//  UberSwiftUITutorial
+//
+//  Created by Jose Garcia on 4/18/23.
+//
+
+import SwiftUI
+import Firebase
+import FirebaseFirestoreSwift
+
+class HomeViewModel: ObservableObject {
+    
+    init() {
+        fetchDrivers()
+    }
+    
+    func fetchDrivers() {
+        Firestore.firestore().collection("users").whereField("accountType", isEqualTo: AccountType.driver.rawValue).getDocuments { snapshot, _ in
+            guard let documents = snapshot?.documents else { return }
+            let drivers = documents.map({ try? $0.data(as: User.self) })
+            
+            print("DEBUG: Drivers \(drivers)")
+        }
+    }
+}
