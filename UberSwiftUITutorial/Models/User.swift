29cb54c0c43f93a4727c62fda934a360c21ee583
chavalk <chavalk@hotmail.com>
2023-04-18 09:38:10 -0500

    Add account type enum and account type property to user model.

--- UberSwiftUITutorial/Models/User.swift
+++ UberSwiftUITutorial/Models/User.swift
@@ -7,10 +7,16 @@
 
 import Foundation
 
+enum AccountType: Int, Codable {
+    case passenger
+    case driver
+}
+
 struct User: Codable {
     let fullName: String
     let email: String
     let uid: String
+    var accountType: AccountType
     var homeLocation: SavedLocation?
     var workLocation: SavedLocation?
 }
