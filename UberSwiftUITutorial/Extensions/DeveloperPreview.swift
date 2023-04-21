455100ce91476d96b2f3f1e848187cacd6c09a54
chavalk <chavalk@hotmail.com>
2023-04-20 23:29:35 -0500

    Add mock trip to developer preview.

--- UberSwiftUITutorial/Extensions/DeveloperPreview.swift
+++ UberSwiftUITutorial/Extensions/DeveloperPreview.swift
@@ -17,5 +17,21 @@ extension PreviewProvider {
 class DeveloperPreview {
     static let shared = DeveloperPreview()
     
+    let mockTrip = Trip(
+        id: NSUUID().uuidString,
+        passengerUid: NSUUID().uuidString,
+        driverUid: NSUUID().uuidString,
+        passengerName: "Jose Garcia",
+        driverName: "John Doe",
+        passengerLocation: .init(latitude: 37.123, longitude: -122.1),
+        driverLocation: .init(latitude: 37.123, longitude: -122.1),
+        pickUpLocationName: "Apple Campus",
+        dropOffLocationName: "Starbucks",
+        pickUpLocationAddress: "123 Main St, Palo Alto CA",
+        pickUpLocation: .init(latitude: 37.456, longitude: -122.15),
+        dropOffLocation: .init(latitude: 37.042, longitude: -122.2),
+        tripCost: 47.0
+    )
+    
     let mockUser = User(fullName: "Jose Garcia", email: "test@gmail.com", uid: NSUUID().uuidString, coordinates: GeoPoint(latitude: 37.38, longitude: -122.05), accountType: .passenger)
 }
