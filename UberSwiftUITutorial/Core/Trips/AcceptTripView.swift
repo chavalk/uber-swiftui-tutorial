393ae7bbe96848564f6df4f6bac85fb53029f4b4
chavalk <chavalk@hotmail.com>
2023-04-21 10:15:12 -0500

    Add bottom padding to action buttons, corner radius, and shadow to entire view.

--- UberSwiftUITutorial/Core/Trips/AcceptTripView.swift
+++ UberSwiftUITutorial/Core/Trips/AcceptTripView.swift
@@ -170,8 +170,11 @@ struct AcceptTripView: View {
             }
             .padding(.top)
             .padding(.horizontal)
+            .padding(.bottom, 24)
         }
         .background(Color.theme.backgroundColor)
+        .cornerRadius(16)
+        .shadow(color: Color.theme.secondaryBackgroundColor, radius: 20)
     }
 }
 
