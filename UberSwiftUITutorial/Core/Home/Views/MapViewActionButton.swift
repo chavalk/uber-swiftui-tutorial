1084922725b57ca5c20faf7148fb6c87e264cefc
chavalk <chavalk@hotmail.com>
2023-04-23 01:00:57 -0500

    Add missing cases to switch statements.

--- UberSwiftUITutorial/Core/Home/Views/MapViewActionButton.swift
+++ UberSwiftUITutorial/Core/Home/Views/MapViewActionButton.swift
@@ -36,7 +36,7 @@ struct MapViewActionButton: View {
             showSideMenu.toggle()
         case .searchingForLocation:
             mapState = .noInput
-        case .locatitonSelected, .polylineAdded:
+        case .locatitonSelected, .polylineAdded, .tripRejected, .tripAccepted, .tripRequested:
             mapState = .noInput
             viewModel.selectedUberLocation = nil
         }
@@ -46,7 +46,7 @@ struct MapViewActionButton: View {
         switch state {
         case .noInput:
             return "line.3.horizontal"
-        case .searchingForLocation, .locatitonSelected, .polylineAdded:
+        case .searchingForLocation, .locatitonSelected, .polylineAdded, .tripRejected, .tripAccepted, .tripRequested:
             return "arrow.left"
 //        default:
 //            return "line.3.horizontal"
--- UberSwiftUITutorial/Core/Home/Views/UberMapViewRepresentable.swift
+++ UberSwiftUITutorial/Core/Home/Views/UberMapViewRepresentable.swift
@@ -41,6 +41,8 @@ struct UberMapViewRepresentable: UIViewRepresentable {
             break
         case .polylineAdded:
             break
+        default:
+            break
         }
     }
     
